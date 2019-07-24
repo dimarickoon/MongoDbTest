5dbbe1a5e713c63e78d3c92e0348d68c3d781065
dimarickoon <appledimarickoon@gmail.com>
2019-07-24 12:39:31 +0300

    added promise module

--- index.js
+++ index.js
@@ -3,31 +3,34 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '901936923:AAFGeTaEcZtlhNCpRkJHo4zvORJmo-dHpCM';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on("text", (message) =>{
-  if(message.text.toLowerCase().indexOf("hi") === 0){
-    bot.sendMessage(message.chat.id, "Hello comrade!");
-  }
-});
 
 var MongoClient = require('mongodb').MongoClient;
 
 var url = 'mongodb://localhost/';
+res = null
+var getData = async function(){
+  MongoClient.connect(url, function(err, db) {
+    console.log("connected");
+    var dbo = db.db("test");
+    dbo.collection("Employers").findOne({}, (function(err, result) {
+      if (err) throw err;
+      db.close();
+      return result.name;
+    })
+  );
+  //var cursor = db.collection('pogs').find   
+  });
+}
+//}
 
-MongoClient.connect(url, function(err, db) {
-  console.log("connected");
-  var dbo = db.db("test");
-  /*var newobj = {name: "dima", surname: "mai"};
-  dbo.collection("Employers").insertOne(newobj, function(err, res) {
-    if (err) throw err;
-    console.log("new document inserted");
-    db.close();
-  });
-  */
-  dbo.collection("Employers").findOne({}, function(err, result) {
-    if (err) throw err;
-    console.log(result.name);
-    db.close();
-  });
-  //var cursor = db.collection('pogs').find
-   
+//mess = getData();
+//console.log(mess);
+
+bot.on("text", (message) =>{
+  if(message.text.toLowerCase().indexOf("hi") === 0){
+    res = await getData();
+    console.log("res : " + res);
+    bot.sendMessage(message.chat.id, "hello");
+    console.log("res : " + res);
+  }
 });
